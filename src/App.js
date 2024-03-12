d675df72d8c5d1dbca04482180ee532eece70511
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-12 09:54:52 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Review from './Review';
 function App() {
   return <main>
-    <div className="container">
+    <section className="container">
       <div className="title">
         <h2>Our reviews</h2>
         <div className="underline"></div>
       </div> 
-    </div>
+    </section>
   </main>
 }
 
