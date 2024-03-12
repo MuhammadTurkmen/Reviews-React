982e69b879a901fc99949c6a46a0fa4ea2ab1c44
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-12 10:04:30 +0430

    init

--- src/Review.js
+++ src/Review.js
@@ -5,8 +5,8 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0)
   const {name, job, image, text} = people[index]
-  return <article>
-    
+  return <article className='review'>
+
   </article>
 }; 
 
