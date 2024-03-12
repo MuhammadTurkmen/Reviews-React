d7c874121dc6b30a6931b8b65878551323d645ed
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-12 10:20:43 +0430

    init

--- src/Review.js
+++ src/Review.js
@@ -19,6 +19,12 @@ const Review = () => {
       <button className='prev-btn'>
         <FaChevronLeft />
       </button>
+      <button className='prev-btn'>
+        <FaChevronLeft />
+      </button>
+      <button className='prev-btn'>
+        <FaChevronLeft />
+      </button>
     </div>
   </article>
 }; 
