ddea5db4334ee5f3c0cc501b3a0cb68778767fc9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-12 09:52:05 +0430

    init

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,9 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  return <main>
+    
+  </main>
 }; 
 
 export default Review;
