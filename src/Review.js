93e8cc6d3e5ea2d4b2712b884aaf3f50b8936c6c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-12 09:59:58 +0430

    init

--- src/Review.js
+++ src/Review.js
@@ -37,7 +37,7 @@ const Review = () => {
     if(randomNumber === index) {
       randomNumber = index + 1 
     } 
-    setIndex(checkNumber(randomNumber)) 
+    setIndex(checkNumber(randomNumber))  
   } 
    
   return <article className='review'>   
